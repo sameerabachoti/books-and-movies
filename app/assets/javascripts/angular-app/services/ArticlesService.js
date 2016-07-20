54f5d9ba1c627661940e967f11698ad1765020d6
sameera91 <sameera.bachoti@gmail.com>
2016-07-19 20:07:58 -0400

    deleted templates folder

--- app/assets/javascripts/angular-app/services/ArticlesService.js
+++ app/assets/javascripts/angular-app/services/ArticlesService.js
@@ -2,7 +2,7 @@ function ArticlesService($http){
   //returns the entire JSON object
   //console.log($http.get("http://localhost:3000/articles.json"));
   this.getMostViewedArticles = function (){
-    return $http.get("http://localhost:3000/articles.json");
+    return $http.get("/articles.json");
   }
 }
 
