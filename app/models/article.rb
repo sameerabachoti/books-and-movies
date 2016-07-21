9b101a777f36d506686cd1b58d7cd5b3c3b7fcda
sameera91 <sameera.bachoti@gmail.com>
2016-07-21 15:11:30 -0400

    generate comments model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,6 @@
 class Article < ActiveRecord::Base
+  has_many :comments
+
   def self.get_articles
     response = HTTParty.get("http://api.nytimes.com/svc/topstories/v1/home.json?api-key=087609e876114f51890cbd02598c33fc")
     response["results"].first(20).each do |article|
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160721155641) do
+ActiveRecord::Schema.define(version: 20160721190957) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -23,6 +23,14 @@ ActiveRecord::Schema.define(version: 20160721155641) do
     t.string   "material_type_facet"
     t.datetime "created_at",          null: false
     t.datetime "updated_at",          null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "content"
+    t.integer  "user_id"
+    t.integer  "article_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
