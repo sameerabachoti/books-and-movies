0b8c664ce4891a78b0cbcffad6580f1fb8515cc5
sameera91 <sameera.bachoti@gmail.com>
2016-07-25 11:58:32 -0400

    filter all articles in the business day section

--- app/assets/javascripts/angular-app/app.js
+++ app/assets/javascripts/angular-app/app.js
@@ -12,5 +12,22 @@ angular
                 templateUrl: 'article.html',
                 controller: 'ArticleController as article'
             })
+            .state('businessDay', {
+                url: '/businessDay',
+                templateUrl: 'businessDay.html',
+                controller: function ($stateParams, ArticlesService, $scope, $filter) {
+                    ArticlesService.getMostViewedArticles().then(
+                        function(result) {
+                           $scope.articles = result.data;
+                           $scope.search = 'Business Day';
+ 
+                           $scope.filteredArticles = $filter('filter')($scope.articles, $scope.search);
+
+                           console.log($scope.filteredArticles);
+                        }
+                    );
+              
+                }
+            });
     });
 
