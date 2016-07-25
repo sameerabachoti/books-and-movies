fc996493207f26b9b86b68895fac2bee90d1c7bc
sameera91 <sameera.bachoti@gmail.com>
2016-07-25 12:49:18 -0400

    change to find_or_create_by method

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,15 +4,8 @@ class Article < ActiveRecord::Base
   def self.get_articles
     response = HTTParty.get("http://api.nytimes.com/svc/topstories/v1/home.json?api-key=***REMOVED***")
     response["results"].first(20).each do |article|
-      new_article = self.new
-      new_article.title = article["title"]
-      new_article.section = article["section"]
-      new_article.abstract = article["abstract"]
-      new_article.url = article["url"]
-      new_article.byline = article["byline"]
-      new_article.published_date = article["published_date"]
-      new_article.material_type_facet = article["material_type_facet"]
-      new_article.save
+      new_article = self.find_or_create_by(title: article["title"], section: article["section"], abstract: article["abstract"], url: article["url"], 
+      byline: article["byline"], published_date: article["published_date"], material_type_facet: article["material_type_facet"])
     end
   end
 end
