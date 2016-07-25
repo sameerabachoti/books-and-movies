da844a70a7c5bb9efdf0dc479d9416b7b9b19eb7
sameera91 <sameera.bachoti@gmail.com>
2016-07-25 13:56:40 -0400

    change to nested view

--- app/assets/javascripts/angular-app/app.js
+++ app/assets/javascripts/angular-app/app.js
@@ -22,8 +22,36 @@ angular
                            $scope.search = 'Business Day';
  
                            $scope.filteredArticles = $filter('filter')($scope.articles, $scope.search);
-
-                           console.log($scope.filteredArticles);
+                        }
+                    );
+              
+                }
+            })
+            .state('home.technology', {
+                url: '/technology',
+                templateUrl: 'technology.html',
+                controller: function ($stateParams, ArticlesService, $scope, $filter) {
+                    ArticlesService.getMostViewedArticles().then(
+                        function(result) {
+                           $scope.articles = result.data;
+                           $scope.search = 'technology';
+ 
+                           $scope.filteredArticles = $filter('filter')($scope.articles, $scope.search);
+                        }
+                    );
+              
+                }
+            })
+            .state('united-states', {
+                url: '/united-states',
+                templateUrl: 'us.html',
+                controller: function ($stateParams, ArticlesService, $scope, $filter) {
+                    ArticlesService.getMostViewedArticles().then(
+                        function(result) {
+                           $scope.articles = result.data;
+                           $scope.search = 'U.S.';
+ 
+                           $scope.filteredArticles = $filter('filter')($scope.articles, $scope.search);
                         }
                     );
               
