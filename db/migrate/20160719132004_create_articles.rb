ff4915471eb400d222dd42db95143d75c10d5f98
sameera91 <sameera.bachoti@gmail.com>
2016-07-26 17:59:52 -0400

    add user_id to articles table

--- db/migrate/20160719132004_create_articles.rb
+++ db/migrate/20160719132004_create_articles.rb
@@ -8,6 +8,7 @@ class CreateArticles < ActiveRecord::Migration
       t.string :byline
       t.string :published_date
       t.string :material_type_facet
+      t.integer :user_id
 
       t.timestamps null: false
     end
