7b9cceb388c16b49ccedad6806adecf6965a0f7b
sameera91 <sameera.bachoti@gmail.com>
2016-07-26 17:59:04 -0400

    add user to articles serializer

--- app/serializers/article_serializer.rb
+++ app/serializers/article_serializer.rb
@@ -1,4 +1,5 @@
 class ArticleSerializer < ActiveModel::Serializer
   attributes :id, :title, :section, :abstract, :url, :byline, :published_date, :material_type_facet
   has_many :comments
+  belongs_to :user
 end
