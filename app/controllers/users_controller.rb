fbb777282d7751e9341b2620e7f435fe21a3cfef
sameera91 <sameera.bachoti@gmail.com>
2016-07-26 17:56:44 -0400

    render user data as JSON

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,17 @@
 class UsersController < ApplicationController
+  skip_before_action :verify_authenticity_token
 
   def index
   end
 
   def show
+    @user = User.find(params[:id])
+
+    respond_to do |format|
+      format.html { render :show }
+      format.json { render json: @user}
+      format.json { render json: @articles }
+      format.json { render json: @articles["user"] }
+    end
   end
 end
