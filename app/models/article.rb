399ea26d6405f520872c440fa1cc597ebda5680b
sameera91 <sameera.bachoti@gmail.com>
2016-07-26 17:58:01 -0400

    specify assocation between users and articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,6 @@
 class Article < ActiveRecord::Base
   has_many :comments
+  belongs_to :user
 
   def self.get_articles
     response = HTTParty.get("http://api.nytimes.com/svc/topstories/v1/home.json?api-key=087609e876114f51890cbd02598c33fc")
