d5faa960089e4316335d8bf08e20ed419d00aced
sameera91 <sameera.bachoti@gmail.com>
2016-07-26 17:55:02 -0400

    add create action to articles controller to post an article to user's JSON data

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def index
     articles = Article.get_articles
     @articles = Article.all
@@ -16,8 +18,21 @@ class ArticlesController < ApplicationController
       format.html { render :show }
       format.json { render json: @article}
       format.json { render json: @comments }
-      format.json { render json: @comments["user"] }
+      format.json { render json: @user }
     end
+  end
+
+  def create
+    @user = User.find(params[:user_id])
+    @article = Article.create(article_params);
+    @article = @user.articles.build(article_params)
+    @article.user = @user;
+    @article.save
+    render :json => @article
+  end
+
+  def article_params
+      params.require(:article).permit(:title, :section, :abstract, :url, :byline, :published_date, :material_type_facet, :user_id)
   end
 
 end
