af0332dbf3a23f9b0f9a8cc3eab30edb8c78efb2
sameera91 <sameera.bachoti@gmail.com>
2016-07-26 17:58:01 -0400

    specify assocation between users and articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,6 @@
 class Article < ActiveRecord::Base
   has_many :comments
+  belongs_to :user
 
   def self.get_articles
     response = HTTParty.get("http://api.nytimes.com/svc/topstories/v1/home.json?api-key=***REMOVED***")
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   has_many :comments
+  has_many :articles
   
   attr_accessor :login
   devise :database_authenticatable, :registerable,
