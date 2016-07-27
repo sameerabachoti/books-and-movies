f422ed65a51dd1e6887b657f09a8e492b875d8b0
sameera91 <sameera.bachoti@gmail.com>
2016-07-27 13:13:00 -0400

    remove comment from article controller

--- app/assets/javascripts/angular-app/controllers/ArticleController.js
+++ app/assets/javascripts/angular-app/controllers/ArticleController.js
@@ -2,20 +2,13 @@ function ArticleController($scope, $stateParams, ArticlesService){
     var ctrl = this;
     ctrl.data = ArticlesService.getArticle($stateParams.id).$$state;
     ctrl.currentUser = currentUser;
-    
+
     ctrl.submitComment = function(){
       var comment = {article_id: $stateParams.id, content: ctrl.commentContent};
       ArticlesService.postComment(comment, $stateParams.id).then(function(response) {
         ctrl.data.value.data.comments.push(comment)
       })
     }
-
-    /*ctrl.bookmarkArticle = function(){
-      var article = ctrl.data.value.data;
-      ArticlesService.postArticle(article).then(function(response){
-        currentUser.articles.push(article);
-      });
-    }*/
 };
 
 ArticleController.$inject = ['$scope', '$stateParams', 'ArticlesService'];
