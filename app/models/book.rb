7dfdbbdd8ab0b35b2e020195223fbb59a6169378
sameera91 <sameera.bachoti@gmail.com>
2016-07-28 13:32:16 -0400

    add comments and user to book model

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,6 @@
 class Book < ActiveRecord::Base
+  has_many :comments
+  belongs_to :user
 
   def self.get_books
     response = HTTParty.get("http://api.nytimes.com/svc/books/v2/lists/combined-print-and-e-book-fiction.json?api-key=087609e876114f51890cbd02598c33fc")
