47eb9426a0a94ee102cf8a716836e2511f8d38e6
sameera91 <sameera.bachoti@gmail.com>
2016-07-27 20:39:46 -0400

    edited book model to display correct information from api

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,13 +1,9 @@
 class Book < ActiveRecord::Base
-  has_many :reviews
-  belongs_to :user
 
   def self.get_books
     response = HTTParty.get("http://api.nytimes.com/svc/books/v2/lists/combined-print-and-e-book-fiction.json?api-key=087609e876114f51890cbd02598c33fc")
     response["results"].first(20).each do |book|
-      new_book = self.find_or_create_by(title: book["book_details"], description: book["description"], contributor: book["contributor"],
-        author: book["author"], contributor_note: book["contributor_note"], price: book["price"], age_group: book["age_group"], publisher: book["publisher"],
-        primary_isbn13: book["primary_isbn13"], primary_isbn10: book["primary_isbn10"])
+      new_book = self.find_or_create_by(title: book["book_details"][0]["title"], description: book["book_details"][0]["description"], contributor: book["book_details"][0]["contributor"], author: book["book_details"][0]["author"], contributor_note: book["book_details"][0]["contributor_note"], price: book["book_details"][0]["price"], age_group: book["book_details"][0]["age_group"], publisher: book["book_details"][0]["publisher"], primary_isbn13: book["book_details"][0]["primary_isbn13"], primary_isbn10: book["book_details"][0]["primary_isbn10"])
     end
   end
 end
