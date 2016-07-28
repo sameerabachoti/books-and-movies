c2a944109f6e4036ad093c37a5634949baed23e5
sameera91 <sameera.bachoti@gmail.com>
2016-07-28 13:26:08 -0400

    add submit comment function to book controller

--- app/assets/javascripts/angular-app/controllers/BookController.js
+++ app/assets/javascripts/angular-app/controllers/BookController.js
@@ -1,19 +1,20 @@
 function BookController($scope, $stateParams, BooksService){
     var ctrl = this;
+
     BooksService.getBook($stateParams.id).then(function(response){   
          ctrl.data = response.data;
-         console.log(ctrl.data);
     });
+    
     ctrl.currentUser = currentUser;
 
-    /*ctrl.submitComment = function(){
-      var comment = {article_id: $stateParams.id, content: ctrl.commentContent};
-      ArticlesService.postComment(comment, $stateParams.id).then(function(response) {
+    ctrl.submitComment = function(){
+      var comment = {book_id: $stateParams.id, content: ctrl.commentContent};
+      BooksService.postComment(comment, $stateParams.id).then(function(response) {
         ctrl.data.value.data.comments.push(comment)
       })
     }
 
-    ctrl.bookmarkArticle = function(){    
+    /*ctrl.bookmarkArticle = function(){    
        var article = ctrl.data.value.data;   
        ArticlesService.postArticle(article).then(function(response){   
          currentUser.articles.push(article);   
