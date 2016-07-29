b6ef6eb918679d8b84980e002b71bd84353975ae
sameera91 <sameera.bachoti@gmail.com>
2016-07-29 12:57:11 -0400

    add books to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   has_many :movie_reviews
   has_many :movies
+  has_many :books
   
   attr_accessor :login
   devise :database_authenticatable, :registerable,
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :email
   has_many :movies
+  has_many :books
 end
