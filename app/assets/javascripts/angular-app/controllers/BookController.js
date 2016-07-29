d576873d99a294e34fc26d24479dd8ad9ff9c937
sameera91 <sameera.bachoti@gmail.com>
2016-07-29 12:53:38 -0400

    edit book template and controller

--- app/assets/javascripts/angular-app/controllers/BookController.js
+++ app/assets/javascripts/angular-app/controllers/BookController.js
@@ -1,25 +1,21 @@
 function BookController($scope, $stateParams, BooksService){
     var ctrl = this;
-
-    BooksService.getBook($stateParams.id).then(function(response){   
-         ctrl.data = response.data;
-    });
-    
+    ctrl.data = BooksService.getBook($stateParams.id).$$state;
     ctrl.currentUser = currentUser;
-
-    ctrl.submitComment = function(){
-      var comment = {book_id: $stateParams.id, content: ctrl.commentContent};
-      BooksService.postComment(comment, $stateParams.id).then(function(response) {
-        ctrl.data.value.data.comments.push(comment)
+    
+    ctrl.submitReview = function(){
+      var review = {book_id: $stateParams.id, content: ctrl.reviewContent};
+      BooksService.postBookReview(review, $stateParams.id).then(function(response) {
+        ctrl.data.value.data.book_reviews.push(review)
       })
     }
 
-    /*ctrl.bookmarkArticle = function(){    
-       var article = ctrl.data.value.data;   
-       ArticlesService.postArticle(article).then(function(response){   
-         currentUser.articles.push(article);   
+    ctrl.bookmarkBook = function(){    
+       var book = ctrl.data.value.data;   
+       BooksService.postBook(book).then(function(response){   
+         currentUser.books.push(book);   
        });   
-    }*/
+    }
 };
 
 BookController.$inject = ['$scope', '$stateParams', 'BooksService'];
