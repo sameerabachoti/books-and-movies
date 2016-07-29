550c50fca958541c68469e083af69f7a78654eef
sameera91 <sameera.bachoti@gmail.com>
2016-07-29 12:58:28 -0400

    add book_reviews table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160727191221) do
+ActiveRecord::Schema.define(version: 20160729140416) do
+
+  create_table "book_reviews", force: :cascade do |t|
+    t.string   "content"
+    t.integer  "user_id"
+    t.integer  "book_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "books", force: :cascade do |t|
     t.string   "title"
