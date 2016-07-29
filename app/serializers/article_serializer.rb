cf4e44570ac117d7d587324bf1fa66afb0b42e4e
sameera91 <sameera.bachoti@gmail.com>
2016-07-28 20:51:39 -0400

    deleted all article and comment files

--- app/serializers/article_serializer.rb
+++
@@ -1,5 +0,0 @@
-class ArticleSerializer < ActiveModel::Serializer
-  attributes :id, :title, :section, :abstract, :url, :byline, :published_date, :material_type_facet
-  has_many :comments
-  belongs_to :user
-end
