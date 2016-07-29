5fa0495da163631b78aab2c2e9f05c0d068c1de1
sameera91 <sameera.bachoti@gmail.com>
2016-07-29 14:52:32 -0400

    add nested views

--- app/assets/javascripts/angular-app/app.js
+++ app/assets/javascripts/angular-app/app.js
@@ -42,36 +42,6 @@ angular
               
                 }
             })
-            /*.state('home.technology', {
-                url: '/technology',
-                templateUrl: 'technology.html',
-                controller: function ($stateParams, ArticlesService, $scope, $filter) {
-                    ArticlesService.getMostViewedArticles().then(
-                        function(result) {
-                           $scope.articles = result.data;
-                           $scope.search = 'technology';
- 
-                           $scope.filteredArticles = $filter('filter')($scope.articles, $scope.search);
-                        }
-                    );
-              
-                }
-            })
-            .state('united-states', {
-                url: '/united-states',
-                templateUrl: 'us.html',
-                controller: function ($stateParams, ArticlesService, $scope, $filter) {
-                    ArticlesService.getMostViewedArticles().then(
-                        function(result) {
-                           $scope.articles = result.data;
-                           $scope.search = 'U.S.';
- 
-                           $scope.filteredArticles = $filter('filter')($scope.articles, $scope.search);
-                        }
-                    );
-              
-                }
-            })*/
             .state('my-movies', {
                 url: '/my-movies',
                 templateUrl: 'myMovies.html',
