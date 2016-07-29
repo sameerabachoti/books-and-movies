b5d1e9911fa5ddfb26550c7d441c6a3ae09b84c9
sameera91 <sameera.bachoti@gmail.com>
2016-07-29 10:17:52 -0400

    change fields in book serializer

--- app/serializers/book_serializer.rb
+++ app/serializers/book_serializer.rb
@@ -1,5 +1,5 @@
 class BookSerializer < ActiveModel::Serializer
-  attributes :id, :title, :description, :contributor, :author, :contributor_note, :price, :age_group, :publisher, :primary_isbn13, :primary_isbn10
-  has_many :comments
+  attributes :id, :title, :description, :contributor, :author, :price, :publisher, :list_name, :weeks_on_list
+  has_many :book_reviews
   belongs_to :user
 end
