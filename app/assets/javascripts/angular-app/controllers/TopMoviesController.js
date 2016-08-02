1b7dfa97ed8e9c5753a6419f22197f53e85df0f1
sameera91 <sameera.bachoti@gmail.com>
2016-08-02 13:37:21 -0400

    function to add a new movie

--- app/assets/javascripts/angular-app/controllers/TopMoviesController.js
+++ app/assets/javascripts/angular-app/controllers/TopMoviesController.js
@@ -1,6 +1,16 @@
-function TopMoviesController(movies){
+function TopMoviesController(MoviesService){
   var ctrl = this;
-  ctrl.data = movies.getMovies().$$state;
+  ctrl.data = MoviesService.getMovies().$$state;
+
+  ctrl.addMovie = function(){
+     console.log("worked");
+      var movie = {display_title: ctrl.displayTitle, summary_short: ctrl.summaryShort};
+      console.log(movie);
+      MoviesService.postNewMovie(movie).then(function(response) {
+        ctrl.data.value.data.push(movie);
+      })
+    }
+
 };
 
 TopMoviesController.$inject = ['MoviesService'];
