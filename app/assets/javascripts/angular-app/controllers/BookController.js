ae77efeaec9927cc2b2d6906ef9ea808c339cba5
sameera91 <sameera.bachoti@gmail.com>
2016-08-02 13:36:40 -0400

    add rating to book review

--- app/assets/javascripts/angular-app/controllers/BookController.js
+++ app/assets/javascripts/angular-app/controllers/BookController.js
@@ -4,7 +4,7 @@ function BookController($scope, $stateParams, BooksService){
     ctrl.currentUser = currentUser;
     
     ctrl.submitReview = function(){
-      var review = {book_id: $stateParams.id, content: ctrl.reviewContent};
+      var review = {book_id: $stateParams.id, content: ctrl.reviewContent, rating: ctrl.reviewRating};
       BooksService.postBookReview(review, $stateParams.id).then(function(response) {
         ctrl.data.value.data.book_reviews.push(review)
       })
