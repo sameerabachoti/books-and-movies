a04813ccf176170d3aa5187c91b3c22b49d1269b
sameera91 <sameera.bachoti@gmail.com>
2016-08-04 23:19:18 -0400

    add environment variable

--- app/models/book.rb
+++ app/models/book.rb
@@ -10,7 +10,7 @@ class Book < ActiveRecord::Base
   end
 
   def self.get_nonfiction_books
-    response2 = HTTParty.get("http://api.nytimes.com/svc/books/v2/lists/combined-print-and-e-book-nonfiction.json?api-key=#{API_KEY}")
+    response2 = HTTParty.get("http://api.nytimes.com/svc/books/v2/lists/combined-print-and-e-book-nonfiction.json?api-key=#{ENV["API_KEY"]}")
     response2["results"].first(10).each do |book|
       nonfiction_book = self.find_or_create_by(title: book["book_details"][0]["title"], description: book["book_details"][0]["description"], contributor: book["book_details"][0]["contributor"], author: book["book_details"][0]["author"], price: book["book_details"][0]["price"], publisher: book["book_details"][0]["publisher"], list_name: book["list_name"], weeks_on_list: book["weeks_on_list"], image: book["book_details"][0]["book_image"])
     end
