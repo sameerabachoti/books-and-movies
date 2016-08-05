da59e1519bd1f098f926f28196aa0fee283bd020
sameera91 <sameera.bachoti@gmail.com>
2016-08-04 23:19:18 -0400

    add environment variable

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,10 +1,10 @@
 class Movie < ActiveRecord::Base
   has_many :movie_reviews
   belongs_to :user
+  belongs_to :creator, :class_name => "User"
 
   def self.get_movies
     response = HTTParty.get("http://api.nytimes.com/svc/movies/v2/reviews/all.json?api-key=#{ENV["API_KEY"]}")
-    binding.pry
     response["results"].first(20).each do |movie|
       new_movie = self.find_or_create_by(display_title: movie["display_title"], mpaa_rating: movie["mpaa_rating"], critics_pick: movie["critics_pick"], byline: movie["byline"], headline: movie["headline"], summary_short: movie["summary_short"], opening_date: movie["opening_date"], image: movie["multimedia"]["src"]) 
     end
