5a37698ff255b91efedbe2a7ef723f5c4145c65b
sameera91 <sameera.bachoti@gmail.com>
2016-08-04 23:11:52 -0400

    add user_id to movie review

--- app/assets/javascripts/angular-app/controllers/BookController.js
+++ app/assets/javascripts/angular-app/controllers/BookController.js
@@ -4,7 +4,7 @@ function BookController($scope, $stateParams, BooksService){
     ctrl.currentUser = currentUser;
     
     ctrl.submitReview = function(){
-      var review = {book_id: $stateParams.id, content: ctrl.reviewContent, rating: ctrl.reviewRating};
+      var review = {book_id: $stateParams.id, content: ctrl.reviewContent, rating: ctrl.reviewRating, user_id: currentUser.id};
       BooksService.postBookReview(review, $stateParams.id).then(function(response) {
         ctrl.data.value.data.book_reviews.push(review)
       })
