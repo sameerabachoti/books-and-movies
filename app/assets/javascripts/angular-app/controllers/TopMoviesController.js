0ae18d5f8fa665faf2971017f87799bab1ab904a
sameera91 <sameera.bachoti@gmail.com>
2016-08-04 23:07:10 -0400

    add edit movie function

--- app/assets/javascripts/angular-app/controllers/TopMoviesController.js
+++ app/assets/javascripts/angular-app/controllers/TopMoviesController.js
@@ -1,19 +1,26 @@
-function TopMoviesController(MoviesService){
+function TopMoviesController($stateParams, MoviesService){
   var ctrl = this;
   ctrl.data = MoviesService.getMovies().$$state;
+  ctrl.user = MoviesService.getUser.$$state;
 
   ctrl.addMovie = function(){
-     console.log("worked");
-      var movie = {display_title: ctrl.displayTitle, summary_short: ctrl.summaryShort};
-      console.log(movie);
+      var movie = {display_title: ctrl.displayTitle, mpaa_rating: ctrl.displayRating, summary_short: ctrl.summaryShort};
+
       MoviesService.postNewMovie(movie).then(function(response) {
         ctrl.data.value.data.push(movie);
       })
-    }
+  }
+
+  ctrl.editMovie = function(){
+      var movie = {display_title: ctrl.displayTitle, mpaa_rating: ctrl.displayRating, summary_short: ctrl.summaryShort};
+      MoviesService.editMovie($stateParams.id, movie).then(function(response) {
+        ctrl.data.value.data.push(movie);
+      })
+  }
 
 };
 
-TopMoviesController.$inject = ['MoviesService'];
+TopMoviesController.$inject = ['$stateParams', 'MoviesService'];
 
 angular
     .module('app')
