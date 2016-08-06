bc3b09370f854d0265d1fb6c8fe46fd96f7bc573
sameera91 <sameera.bachoti@gmail.com>
2016-08-05 20:31:27 -0400

    add movie reviews to user serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :email
-  has_many :movies
-  has_many :books
+  attributes :id, :email, :name
+  has_many :movie_reviews
+  has_many :movies                    
 end
