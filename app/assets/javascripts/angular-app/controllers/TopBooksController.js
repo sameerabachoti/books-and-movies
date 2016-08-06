930fc5cf0055cd8839408a46bb351c335fc6806c
sameera91 <sameera.bachoti@gmail.com>
2016-08-05 20:29:48 -0400

    allow user to add new book

--- app/assets/javascripts/angular-app/controllers/TopBooksController.js
+++ app/assets/javascripts/angular-app/controllers/TopBooksController.js
@@ -1,9 +1,17 @@
-function TopBooksController(books){
+function TopBooksController($stateParams, BooksService){
   var ctrl = this;
-  ctrl.data = books.getTopBooks().$$state;
+  ctrl.data = BooksService.getTopBooks().$$state;
+
+  ctrl.addBook = function(){
+      var book = {title: ctrl.title, author: ctrl.author, description: ctrl.description};
+
+      BooksService.postNewBook(book).then(function(response) {
+        ctrl.data.value.data.push(book);
+      })
+  }
 };
 
-TopBooksController.$inject = ['BooksService'];
+TopBooksController.$inject = ['$stateParams', 'BooksService'];
 
 angular
     .module('app')
