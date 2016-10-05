1ae9e6d1a40f08b3398422f0317c411005acdec0
sameera91 <sameera.bachoti@gmail.com>
2016-10-05 17:08:26 -0400

    test

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -5,7 +5,7 @@ class Movie < ActiveRecord::Base
   def self.get_movies
     response = HTTParty.get("http://api.nytimes.com/svc/movies/v2/reviews/all.json?api-key=087609e876114f51890cbd02598c33fc")
   
-    response["results"].first(20).each do |movie|
+    response["results"].first(2).each do |movie|
       new_movie = self.find_or_create_by(display_title: movie["display_title"], mpaa_rating: movie["mpaa_rating"], critics_pick: movie["critics_pick"], byline: movie["byline"], headline: movie["headline"], summary_short: movie["summary_short"], opening_date: movie["opening_date"], image: movie["multimedia"]["src"]) 
     end
   
