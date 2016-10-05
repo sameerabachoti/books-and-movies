5d170a4770aff504ddb152ecb88e1af0ab26806b
sameera91 <sameera.bachoti@gmail.com>
2016-10-05 15:50:11 -0400

    changed application.rb

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,6 @@ module NytHeadlines
     end
     
     config.active_record.raise_in_transactional_callbacks = true
+    config.assets.paths << Rails.root.join("vendor","assets","bower_components","bootstrap-sass-official","assets","fonts")
   end
 end
